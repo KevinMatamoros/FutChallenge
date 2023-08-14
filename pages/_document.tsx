1294320b901a921dbe560485208ee19c861fc162
Kevin Matamoros Serrano <kmatamoros@consultec-ti.com>
2023-08-14 17:22:38 -0600

    Allow navigation between pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html>
@@ -7,7 +7,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <footer className="fixed bottom-0 text-xs w-52 left-0 right-0 mx-auto mb-2">© 2023 TechPV All rights reserved.</footer>
       </body>
     </Html>
-  )
+  );
 }
